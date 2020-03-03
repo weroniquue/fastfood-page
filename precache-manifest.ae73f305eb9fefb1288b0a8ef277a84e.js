1562957c653f2e1ba5d0a698b39c07a8622cb483
weroniquue <weronica97@gmail.com>
2020-03-03 19:59:51 +0100

    Updates

---
+++ precache-manifest.ae73f305eb9fefb1288b0a8ef277a84e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb4f370e68dd1801175f8d5f10b7f39",
+    "url": "https://weroniquue.github.io/fastfood-page /index.html"
+  },
+  {
+    "revision": "e6193045ed307a4f43c1",
+    "url": "https://weroniquue.github.io/fastfood-page /static/css/main.30c5d73e.chunk.css"
+  },
+  {
+    "revision": "14280b7db343969811b7",
+    "url": "https://weroniquue.github.io/fastfood-page /static/js/2.546f0dd3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "https://weroniquue.github.io/fastfood-page /static/js/2.546f0dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6193045ed307a4f43c1",
+    "url": "https://weroniquue.github.io/fastfood-page /static/js/main.c1e3bb4b.chunk.js"
+  },
+  {
+    "revision": "201c0a9ff4fc5aadeafa",
+    "url": "https://weroniquue.github.io/fastfood-page /static/js/runtime-main.4e3c0fb9.js"
+  }
+]);
